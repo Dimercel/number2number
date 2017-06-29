6dafea5a9953a2bb65c4cf2ee91aa1f358901a48
Dimercel <xolcman@gmail.com>
2017-06-29 18:16:03 +0300

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Converter (decToOther, otherToDec)
+import Data.Maybe (fromMaybe)
+
+main = do
+    putStrLn "Enter a number:"
+    x <- getLine
+    putStrLn "Enter a radix of number[2-36]:"
+    baseOrig <- getLine
+    putStrLn "Enter a result radix[2-36]:"
+    baseResult <- getLine
+    putStrLn "Result:"
+
+    let inDec = otherToDec x (read baseOrig :: Int)
+    let result = decToOther (fromMaybe 0 inDec) (read baseResult :: Int)
+
+    case result of
+      (Just x) -> putStrLn x
+      Nothing -> putStrLn "Error!"
---
+++ src/Converter.hs
@@ -0,0 +1,39 @@
+module Converter
+  ( decToOther
+  , otherToDec
+  ) where
+
+import Data.List (length, elemIndex)
+import Data.Maybe (isNothing, fromMaybe)
+
+
+numericSymbols :: String
+numericSymbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+numToSymbol :: Int -> Maybe Char
+numToSymbol x = if x > -1 && x < length numericSymbols
+                then Just $ numericSymbols !! x
+                else Nothing
+
+symbolToNum :: Char -> Maybe Int
+symbolToNum sym = elemIndex sym numericSymbols
+
+getModulos :: Int -> Int -> [Int]
+getModulos 0 _ = []
+getModulos x radix = (x `mod` radix) : getModulos (x `div` radix) radix
+
+decToOther :: Int -> Int -> Maybe String
+decToOther x radix =
+  let numDigits = map numToSymbol $ reverse $ getModulos x radix
+  in if any isNothing numDigits
+     then Nothing
+     else Just $ map (fromMaybe '_') numDigits
+
+otherToDec :: String -> Int -> Maybe Int
+otherToDec x radix =
+        let trans x = zip (reverse $ map symbolToNum x) [0..]
+            pow radix = map (\x -> fmap (* radix ^ snd x) (fst x))
+            powList = pow radix (trans x)
+        in if any isNothing powList
+           then Nothing
+           else Just (sum $ map (fromMaybe 0) powList)
